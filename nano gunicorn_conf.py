de64b7debd2180865da6c7cb02425d57a6d2c08e
viktor-shved <viktor-shved@yandex-team.ru>
2022-08-09 22:50:27 +0300

    dsad

--- nano gunicorn_conf.py
+++ nano gunicorn_conf.py
@@ -1,7 +1,7 @@
 from multiprocessing import cpu_count
 
 # Socket Path
-bind = 'unix:/home/viktor-shved/fastapi_demo/gunicorn.sock'
+bind = 'unix:/home/viktor-shved/nana_fastapi/gunicorn.sock'
 
 # Worker Options
 workers = cpu_count() + 1
@@ -9,5 +9,5 @@ worker_class = 'uvicorn.workers.UvicornWorker'
 
 # Logging Options
 loglevel = 'debug'
-accesslog = '/home/viktor-shved/fastapi_demo/access_log'
-errorlog = '/home/viktor-shved/fastapi_demo/error_log'
+accesslog = '/home/viktor-shved/nana_fastapi/access_log'
+errorlog = '/home/viktor-shved/nana_fastapi/error_log'

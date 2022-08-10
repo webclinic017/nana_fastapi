b70ba5b3990d07aebe64145cc2acfd13e3b7cd50
viktor-shved <viktor-shved@yandex-team.ru>
2022-08-10 23:38:36 +0300

    dsad

--- routers/jobs.py
+++ routers/jobs.py
@@ -45,7 +45,7 @@ def token_required(func):
         token = request.headers.get('authorization')
         if token:
             if not settings.wms_token in token.split():
-                raise RequestValidationError('Wrong token')
+                raise RequestValidationError(f'Wrong token {token}')
         # my_header will be now available in decorator
         return await func(*args, request, **kwargs)
     return wrapper

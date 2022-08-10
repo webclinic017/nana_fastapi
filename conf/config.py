5307bb6b9e6389e41ff0275b6206fcfafb21ca66
viktor-shved <viktor-shved@yandex-team.ru>
2022-08-10 23:52:34 +0300

    dsad

--- conf/config.py
+++ conf/config.py
@@ -1,13 +1,14 @@
 from pydantic import BaseSettings
 from functools import lru_cache
 import os
-
+wms_token = os.getenv("wms_token", "lol")
+wms_url = os.getenv("wms_url", "lol")
 class Settings(BaseSettings):
     app_name: str = "Awesome API"
     admin_email: str = '1@1.ru'
     items_per_user: int = 50
-    wms_token: str = os.getenv("wms_token", "lol")
-    wms_url: str = os.getenv("wms_url", "lol")
+    wms_token: str = wms_token
+    wms_url: str = wms_url
     DB_PASSWORD: str = 'test'
     DB_NAME: str = 'stub'
     DB_USER: str = 'taxi'
